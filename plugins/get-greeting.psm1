79ed02dea74ed913ab988e354e885f6c9723b9c1
Tom Woodling <t.woodling@coolblue.nl>
2018-08-30 13:39:23 +0200

    github is still dumb

---
+++ plugins/get-greeting.psm1
@@ -0,0 +1,15 @@
+function get-greeting {
+    [cmdletbinding()]
+[PoshBot.BotCommand(
+    CommandName = 'get-greeting',
+    Aliases = ('hi', 'hello'),
+    Permissions = 'read'
+)]
+param()
+
+$greetz = @('hi there :bowtie:','good day :sun_with_face:','nice to see you :robot_face:','yo :')
+
+$outp = $greetz | Get-Random
+
+Write-Output $outp
+}
