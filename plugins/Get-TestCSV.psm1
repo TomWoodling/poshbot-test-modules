7f6b3e555857b678e2888aa959f935ffd4039689
Tom Woodling <t.woodling@coolblue.nl>
2018-09-11 10:14:17 +0200

    updated tests

--- plugins/Get-TestCSV.psm1
+++ plugins/Get-TestCSV.psm1
@@ -22,7 +22,9 @@ function Get-TestCSV {
     try {
         $bips = Get-Service | where servicename -like "*CP*"
         
-        $bips | Export-Csv -NoTypeInformation "$env:BOTNAME\test.csv" -Force
+        $bips | Export-Csv -NoTypeInformation "$env:BOTROOT\test.csv" -Force
+
+        New-PoshBotFileUpload -Path "$env:BOTROOT\test.csv" -Title 'testing.csv' -DM
 
         # Set a successful result
         $result.success = $true
@@ -32,7 +34,7 @@ function Get-TestCSV {
     catch {
 
         $clib = ':cold_sweat:'
-        $result.output = "I cannot get details for $User $clib"
+        $result.output = "I cannot get details for this $clib"
         
         # Set a failed result
         $result.success = $false
@@ -40,6 +42,6 @@ function Get-TestCSV {
     # Return the result and convert it to json, then attach a snippet with the results
 
 
-    New-PoshBotFileUpload -Path "$env:BOTNAME\test.csv" -Title 'testing.csv' -DM
+    return $result.output
 
 }
